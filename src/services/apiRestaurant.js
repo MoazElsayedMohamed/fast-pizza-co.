0f9aec4352f68f4dd2da78933ba67a7320f5d2cb
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-06-06 00:29:15 +0300

    feat: :sparkles: fetching orders

--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -9,3 +9,11 @@ export async function getMenu() {
   const { data } = await res.json();
   return data;
 }
+
+export async function getOrder(id) {
+  const res = await fetch(`${API_URL}/order/${id}`);
+  if (!res.ok) throw Error(`Couldn't find order #${id}`);
+
+  const { data } = await res.json();
+  return data;
+}
