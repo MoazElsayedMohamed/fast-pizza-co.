6b8c644fe921e6779875ccf9c682356a62d653b2
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-06-06 12:50:15 +0300

    feat: :sparkles: writing data with react router actions

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import Home from "./UI/Home";
 import Error from "./UI/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./UI/AppLayout";
 
 const routes = createBrowserRouter([
@@ -20,8 +22,12 @@ const routes = createBrowserRouter([
         errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
-      { path: "/order/new", element: <CreateOrder /> },
-      { path: "/order/:orderId", element: <Order /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      { path: "/order/:orderId", element: <Order />, loader: orderLoader },
     ],
   },
 ]);
--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -17,3 +17,21 @@ export async function getOrder(id) {
   const { data } = await res.json();
   return data;
 }
+
+export async function createOrder(newOrder) {
+  try {
+    const res = await fetch(`${API_URL}/order`, {
+      method: "POST",
+      body: JSON.stringify(newOrder),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!res.ok) throw Error();
+    const { data } = await res.json();
+    return data;
+  } catch {
+    throw Error("Failed creating your order");
+  }
+}
