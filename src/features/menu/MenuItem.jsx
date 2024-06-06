201cb92cd8669001e460b8586de3aac319350c4d
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-06-06 20:10:57 +0300

    feat: :art: style our menu list

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,16 +1,31 @@
+import Button from "../../UI/Button";
 import { formatCurrency } from "../../utils/helpers";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
-        <p>{name}</p>
-        <p>{ingredients.join(", ")}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+    <li className="flex items-center gap-4 py-2">
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`h-36 ${soldOut ? " opacity-70 grayscale" : ""}`}
+      />
+      <div className="text-xl flex flex-col grow pt-0.5">
+        <p className="font-medium">{name}</p>
+        <p className="font-medium capitalize text-stone-500 italic">
+          {ingredients.join(", ")}
+        </p>
+        <div className="mt-10 flex items-center justify-between">
+          {!soldOut ? (
+            <p className="text-sm">{formatCurrency(unitPrice)}</p>
+          ) : (
+            <p className="text-sm font-medium uppercase text-stone-500">
+              Sold out
+            </p>
+          )}
+
+          <Button type="small">Add to Cart</Button>
         </div>
       </div>
     </li>
